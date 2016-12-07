c787672b807aa03c52c2a1088230c1ac92e33f29
celanajaya <petermsteele@gmail.com>
2016-12-07 08:16:15 -0800

    Initial Commit

---
+++ FilterCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FilterCollection
+//
+//  Created by Peter Steele on 12/7/16.
+//  Copyright © 2016 Peter Steele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FilterCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FilterCollection
+//
+//  Created by Peter Steele on 12/7/16.
+//  Copyright © 2016 Peter Steele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
